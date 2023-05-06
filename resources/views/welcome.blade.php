50fd03e4e90ce262eaa99000ce3c9288183ffaab
excelriot <119025323+excelriot@users.noreply.github.com>
2023-05-06 13:12:38 +0700

    tk

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
+@extends('layouts.app') @section('header')
+<div class="container-fluid">
+    <h1 class="text-black-50" style="text-align: center">DATA PEGAWAI</h1>
+</div>
+@endsection
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
